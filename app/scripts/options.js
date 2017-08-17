5dd2773cd38be4e20dbf1e0da089af47fff8bba4
Pg <pg.developper.fr@gmail.com>
2017-08-17 16:07:02 +0200

    Implementation of internationalization

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -4,25 +4,27 @@
 
 
 let optionManager = new OptionManager({
-  like_what: 'subscribed'
-})
+	like_what: 'subscribed'
+});
 
 function onFieldChange() {
-  optionManager.set({
-    [this.name]: this.value
-  })
+	optionManager.set({
+		[this.name]: this.value
+	})
 }
 
 // Restore options
 optionManager.get().then((options) => {
-  document
-      .querySelector(`input[name="like_what"][value="${options.like_what}"]`)
-      .click()
-})
+	document.querySelector(`input[name="like_what"][value="${options.like_what}"]`)
+	.click()
+});
 
 // Trigger a function when the user changes an option
-document
-  .querySelectorAll('input')
-  .forEach((field) => {
-    field.addEventListener( 'click', onFieldChange.bind(field) )
-  })
+document.querySelectorAll('input').forEach((field) => {
+	field.addEventListener( 'click', onFieldChange.bind(field) )
+});
+
+//translate spans with i18n-*
+document.querySelectorAll('span[id^=i18n-]').forEach((el) => {
+	el.innerHTML = browser.i18n.getMessage( el.id.split("i18n-")[1] );
+});
