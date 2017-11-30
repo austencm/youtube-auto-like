47fd0652b72c38838ac646baf9046b575c87cbe3
Pg <pg.developper.fr@gmail.com>
2017-11-30 17:34:40 +0100

    Old youtube support
    - Add old youtube support
    - lang eng for reason explained in previous commit (oups)

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,7 +1,5 @@
-
 // We need to know which version of YouTube we're dealing with
 // The material version has no ID on the body, hence this dumb check
-const IS_MATERIAL = !document.body.id
 const IS_MATERIAL = !document.body.id;
 
 // Create an OptionManager
@@ -9,10 +7,22 @@ let optionManager = new OptionManager(OPTIONS);
 
 // Fetch our options then fire things up
 optionManager.get().then((options) => {
-	let liker = new MaterialLiker(options)
-	/*
-	Hook into one of YouTube's custom events.
-	This fires when page changes, so the liker is run only when needed.
-	 */
-	document.addEventListener('yt-page-data-updated', () => liker.init() );
+	if (IS_MATERIAL) {
+		let liker = new MaterialLiker(options);
+		/*
+		Hook into one of YouTube's custom events.
+		This fires when page changes, so the liker is run only when needed.
+		 */
+		document.addEventListener('yt-page-data-updated', () => liker.init() );
+	} else {
+		let liker = new Liker(options);
+    	/*
+    	Old YouTube is a bit clunkier.
+    	We need to initialize the liker immediately
+    	and also run it whenever the AJAX load finishes.
+    	 */
+	  	liker.init();
+
+		window.addEventListener('spfdone', () => liker.init() );
+	}
 });
