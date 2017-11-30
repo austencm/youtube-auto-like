8ea861245995ceab12e5daf9be7db489f4a813f2
Pg <pg.developper.fr@gmail.com>
2017-11-30 01:58:23 +0100

    Add percentage
    Check the percentage of played video, if over the percentage set by
    user, then attempt like.
    - get the video value
    - calcul % played

--- app/scripts/modules/liker-material.js
+++ app/scripts/modules/liker-material.js
@@ -8,7 +8,7 @@ class MaterialLiker {
 	 *                         whether to like all videos or just subscribed
 	 */
 	constructor(options) {
-		this.options = options
+		this.options = options;
 	}
 
 	reset() {
@@ -24,14 +24,14 @@ class MaterialLiker {
 	waitForButtons(callback) {
 		if (this.icon.like == null) {
 			// get the SVG pattern
-			this.icon.like = document.querySelector('g#like path').getAttribute('d')
-			this.icon.dislike = document.querySelector('g#dislike path').getAttribute('d')
+			this.icon.like = document.querySelector('g#like path').getAttribute('d');
+			this.icon.dislike = document.querySelector('g#dislike path').getAttribute('d');
 		}
 		
 		if (this.icon.like != null) {
 			// Select the like/dislike icons using their SVG data
-			let likeElement = document.querySelectorAll('#menu.ytd-video-primary-info-renderer g path[d="' + this.icon.like +'"]')[0]
-			let dislikeElement = document.querySelectorAll('#menu.ytd-video-primary-info-renderer g path[d="' + this.icon.dislike +'"]')[0]
+			let likeElement = document.querySelectorAll('#menu.ytd-video-primary-info-renderer g path[d="' + this.icon.like +'"]')[0];
+			let dislikeElement = document.querySelectorAll('#menu.ytd-video-primary-info-renderer g path[d="' + this.icon.dislike +'"]')[0];
 			
 			// Make sure both icons exist
 			if (likeElement && dislikeElement) {
@@ -48,12 +48,39 @@ class MaterialLiker {
 	}
 
 	/*
+	 * Wait the number of minutes or % specified by user
+	 */
+	waitTimer(callback) {
+		// if Instant like, direct return to like
+		if (this.options.like_timer == "instant") {
+			callback();
+		}
+		// else continue
+		
+		var video = document.getElementsByClassName('video-stream')[0];
+		let duration = video.duration;
+
+		if (this.options.type_timer == "percentage") {
+			//normally buttons load after video, but if they load before, maybe a bug can happen
+			let percentageAtLike = this.options.timer_value;
+			let nowInPercent = video.currentTime / duration * 100;
+			if (nowInPercent >= percentageAtLike) {
+				callback();
+			} else {
+				setTimeout(() => this.waitTimer(callback), 1000 );
+			}
+		} else if (this.options.type_timer == "minute") {
+			// TO DO
+		}
+	}
+
+	/*
 	 * Take a wild guess
 	 * @return {Boolean} True if the like or dislike button is active
 	 */
 	isVideoRated() {
 		return this.btns.like.parentNode.getAttribute("aria-pressed") ||
-				 this.btns.dislike.parentNode.getAttribute("aria-pressed")
+				 this.btns.dislike.parentNode.getAttribute("aria-pressed");
 	}
 
 	/*
@@ -62,15 +89,15 @@ class MaterialLiker {
 	 *                   the current video's channel
 	 */
 	isUserSubscribed() {
-		let subscribeButton = document.querySelector('#subscribe-button paper-button')
-		return subscribeButton && subscribeButton.hasAttribute('subscribed')
+		let subscribeButton = document.querySelector('#subscribe-button paper-button');
+		return subscribeButton && subscribeButton.hasAttribute('subscribed');
 	}
 
 	/*
 	 * Clickity click the button
 	 */
 	attemptLike() {
-		this.btns.like.click()
+		this.btns.like.click();
 	}
 
 	/*
@@ -88,11 +115,24 @@ class MaterialLiker {
 			 */
 			var isTrueSet = ( (this.isVideoRated() == 'true') || ( this.options.like_what === 'subscribed' && !this.isUserSubscribed() ) );
 			if ( isTrueSet ) {
-				console.log("not liked")
-				return
+				console.log("not liked");
+				return;
 			}
-			this.attemptLike()
-			console.log('liked')
-		})
+			/*
+			Else do the stuff
+			*/
+			this.waitTimer(() => {
+				/*
+				Maybe the use did an action while we was waiting, so check again
+				*/
+				var isTrueSet = ( (this.isVideoRated() == 'true') || ( this.options.like_what === 'subscribed' && !this.isUserSubscribed() ) );
+				if ( isTrueSet ) {
+					console.log("not liked");
+					return;
+				}
+				this.attemptLike();
+				console.log('liked');
+			});
+		});
 	}
 }
