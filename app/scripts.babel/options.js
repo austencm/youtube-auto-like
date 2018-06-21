5db437ff67a65d8ea13231d66b2c6afc6ecc11a5
austencm <austencm@gmail.com>
2018-06-21 13:38:24 -0700

    Add bug report link

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -9,11 +9,29 @@ const defaults = {
 };
 const optionManager = new OptionManager(defaults);
 const i18n = new I18n();
+const bugReportTemplate = `
+> Thanks for reporting! Please fill in the following info:
+
+OS (Windows / MacOS / etc.):
+
+YouTube version (YouTube / YouTube Gaming):
+
+What happened:
+
+What should have happened:
+
+Anything helpful to reproduce the issue:
+
+Current settings (leave this alone):
+`;
 
 i18n.populateText();
 
-// Load options
-optionManager.get().then(options => {
+function serialize(obj) {
+  return Object.keys(obj).map(k => `${encodeURIComponent(k)}: ${encodeURIComponent(obj[k])}`).join(encodeURI(', '));
+}
+
+function populateOptions(options) {
   document
     .querySelectorAll('input')
     .forEach(field => {
@@ -28,7 +46,14 @@ optionManager.get().then(options => {
         field.value = val;
       }
     });
-});
+  // Add options state to report issue link
+  const reportLink = document.querySelector('#report-link');
+  const url = `https://github.com/austencm/youtube-auto-like/issues/new?labels=bug&body=${encodeURI(bugReportTemplate)}${serialize(options)}`;
+  reportLink.setAttribute('href', url);
+}
+
+// Load options
+optionManager.get().then(populateOptions);
 
 // When the user changes an option, save it
 document
@@ -40,5 +65,5 @@ document
       .from((new FormData(e.currentTarget)).entries())
       .forEach(([name, val]) => newOptions[name] = val);
 
-    optionManager.set(newOptions).then(optionManager.get).then(console.log);
+    optionManager.set(newOptions).then(optionManager.get).then(populateOptions);
   });
