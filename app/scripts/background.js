5cad7aa8eae562fba56ab0ea0bb615f17e0c7755
Pg <pg.developper.fr@gmail.com>
2018-09-03 16:55:44 +0200

    Brackground fix & Add patch note
    Add a check that verifiy this is a plugin update and not a firefox update. Firefox update also trigger "onInstall".

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,13 +2,31 @@
  * Script running in background for the plugin
  * Currently detect update, and smooth transition between versions
  */
+function isNewVersion(stored_version, manifest_version) {
+	for (var i = 0; i <= stored_version.split(".").length - 1; i++) {
+		console.log(stored_version.split(".")[i])
+		console.log( manifest_version.split(".")[i])
+		if (stored_version.split(".")[i] < manifest_version.split(".")[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
 function handleInstalled(details) {
-	// reset the storage for tansition from 2.0.3
-	//browser.storage.local.clear();
-	// open update info
-	browser.tabs.create({
-		url: "update_info.html"
-	});
+	let optionManager = new OptionManager(OPTIONS);
+	optionManager.get().then((options) => {
+		// if this is a new version display patch note
+		if ( isNewVersion(options.plugin_version, browser.runtime.getManifest().version) ) {
+				browser.tabs.create({
+				url: "update_info.html"
+			});
+			// save the new version number
+			options.plugin_version = browser.runtime.getManifest().version;
+			optionManager.set(options);
+		}
+		//browser.storage.local.clear();
+	});	
 }
 // triggered when new  version installed
-//browser.runtime.onInstalled.addListener(handleInstalled);
+browser.runtime.onInstalled.addListener(handleInstalled);
--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -2,5 +2,6 @@ var OPTIONS = {
 	like_what: 'subscribed',
 	like_timer: 'instant',
 	type_timer: 'percentage',
-	timer_value: 10
+	timer_value: 10,
+	plugin_version: "0.0.0"
 }
