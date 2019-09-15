e37d442b3c680d862523903183036d77377ecd03
Pg <pg.developper.fr@gmail.com>
2019-09-15 14:21:30 +0200

    Push update to liker.js
    Update the old youtube layout script with the functionnalities of the material one :
    - playlist
    - wait for button
    - wait for video
    - random
    - multiple run playlist
    - desactivated button

--- app/scripts/modules/liker.js
+++ app/scripts/modules/liker.js
@@ -35,6 +35,23 @@ class Liker {
 			setTimeout(() => this.waitForButtons(callback), 1000 );
 	}
 
+		/**
+	* Detects when the video player has loaded
+	* @param  {Function} callback
+	*/
+	waitForVideo(callback) {
+		this.video = document.querySelector('.video-stream');
+		if (this.video) {
+ 			callback();
+		} else {
+			setTimeout(() => this.waitForVideo(callback), 1000);
+		}
+	}
+
+	randomIntFromInterval(min, max) { // min and max included 
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
 	/*
 	 * Wait the number of minutes or % specified by user
 	 */
@@ -42,52 +59,52 @@ class Liker {
 		// if Instant like, direct return to like
 		if (this.options.like_timer == "instant") {
 			callback();
+			return;
 		}
-		// else continue
-		
-		var video = document.getElementsByClassName('video-stream')[0];
-		let duration = video.duration;
+		else if (this.options.like_timer == "random") {
+			var video = document.getElementsByClassName('video-stream')[0];
+			let duration = video.duration;
 
-		if (this.options.type_timer == "percentage") {
 			//normally buttons load after video, but if they load before, maybe a bug can happen
-			let percentageAtLike = this.options.timer_value;
+
 			let nowInPercent = video.currentTime / duration * 100;
-			if (nowInPercent >= percentageAtLike) {
-				callback();
-			} else {
-				setTimeout(() => this.waitTimer(callback), 1000 );
-			}
-		} else if (this.options.type_timer == "minute") {
-			let timeAtLike = this.options.timer_value;
-			// change timeAtLike if vid shorter than time set by user
-			if (video.duration < timeAtLike) {
-				timeAtLike = video.duration;
-			} else {
-				// convert in second
-				timeAtLike *= 60;
-			}
-			if (video.currentTime >= timeAtLike) {
+
+			if (nowInPercent >= this.randomTimerPercent) {
 				callback();
 			} else {
 				setTimeout(() => this.waitTimer(callback), 1000 );
 			}
 		}
-	}
+		else {
+		
+			var video = document.getElementsByClassName('video-stream')[0];
+			let duration = video.duration;
 
-	/**
-	 * Clickity click the button
-	 */
-	attemptLike() {
-			this.btns.like.click();
-
-			/*
-			Confirm the click registered.
-			Sometimes the buttons load before the event
-			handlers get attached and nothing happens.
-			 */
-			if ( !this.isVideoRated() )
-				// Persistence pays off
-				setTimeout(() => this.attemptLike(), 1000 );
+			if (this.options.type_timer == "percentage") {
+				//normally buttons load after video, but if they load before, maybe a bug can happen
+				let percentageAtLike = this.options.timer_value;
+				let nowInPercent = video.currentTime / duration * 100;
+				if (nowInPercent >= percentageAtLike) {
+					callback();
+				} else {
+					setTimeout(() => this.waitTimer(callback), 1000 );
+				}
+			} else if (this.options.type_timer == "minute") {
+				let timeAtLike = this.options.timer_value;
+				// change timeAtLike if vid shorter than time set by user
+				if (video.duration < timeAtLike) {
+					timeAtLike = video.duration;
+				} else {
+					// convert in second
+					timeAtLike *= 60;
+				}
+				if (video.currentTime >= timeAtLike) {
+					callback();
+				} else {
+					setTimeout(() => this.waitTimer(callback), 1000 );
+				}
+			}
+		}
 	}
 
 	/**
@@ -110,39 +127,82 @@ class Liker {
 	}
 
 	/**
+	 * Clickity click the button
+	 */
+	attemptLike() {
+			this.btns.like.click();
+	}
+
+
+	blockMultipleRun() {
+		if (!this.hasOwnProperty("IS_STARTED")) { //if not defined this is the 1st run
+			this.IS_STARTED = true;
+			return;
+		} else {
+			if (this.IS_STARTED) {
+				exit();
+			} else { //could be a new video in playlist
+				this.IS_STARTED = true;
+				return;
+			}
+		}
+	}
+
+	finish() {
+		this.IS_STARTED = false;
+	}
+
+	/**
 	 * Starts the liking magic.
 	 * The liker won't do anything unless this method is called.
 	 */
 	init() {
-		// console.log('initializing...')
+		if (this.options.like_what === "none") {
+			return;
+		}
 
+		this.blockMultipleRun();
 		this.reset();
 		console.log('yt-autolike start');
+
 		this.waitForButtons(() => {
 			/*
 			If the video is already liked/disliked
 			or the user isn't subscribed to this channel,
 			then we don't need to do anything.
 			 */
-			 var isTrueSet = ( (this.isVideoRated() == 'true') || ( this.options.like_what === 'subscribed' && !this.isUserSubscribed() ) );
+			let rated = this.isVideoRated();
+			let isTrueSet = ( rated || ( this.options.like_what === 'subscribed' && !this.isUserSubscribed() ) );
+
 			if ( isTrueSet ) {
-				console.log("not liked");
+				console.log("not liked check 1");
+				this.finish();
 				return;
 			}
 			/*
 			Else do the stuff
 			*/
-			this.waitTimer(() => {
-				/*
-				Maybe the use did an action while we was waiting, so check again
-				*/
-				var isTrueSet = ( (this.isVideoRated() == 'true') || ( this.options.like_what === 'subscribed' && !this.isUserSubscribed() ) );
-				if ( isTrueSet ) {
-					console.log("not liked");
-					return;
-				}
-				this.attemptLike();
-				console.log('liked');
+			// Define a random timer if selected
+			if (this.options.like_timer == "random") {
+				this.randomTimerPercent = this.randomIntFromInterval(0, 99);
+			}
+
+			this.waitForVideo(() => {
+				this.waitTimer(() => {
+					/*
+					Maybe the use did an action while we was waiting, so check again
+					*/
+					let rated = this.isVideoRated();
+					let isTrueSet = ( rated || ( this.options.like_what === 'subscribed' && !this.isUserSubscribed() ) );
+					if ( isTrueSet ) {
+						console.log("not liked check 2");
+						this.finish();
+						return;
+					}
+					this.attemptLike();
+					console.log('liked');
+					this.finish();
+				});
 			});
 		});
 	}
