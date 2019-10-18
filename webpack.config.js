264bc6444ee898ca3fe560691d576bccfec5ea02
austencm <austencm@gmail.com>
2019-10-17 18:39:26 -0700

    Fix webpack removing static files when dev server reloaded

--- webpack.config.js
+++ webpack.config.js
@@ -55,24 +55,18 @@ var options = {
   },
   plugins: [
     // clean the build folder
-    new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: 'build' }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['!manifest.json', '!_locales/**/*']
+    }),
     // expose and write the allowed env vars on the compiled bundle
     new webpack.EnvironmentPlugin(["NODE_ENV", "DEBUG"]),
     new CopyWebpackPlugin([
       {
-        from: "src/manifest.json",
-        transform: function (content, path) {
-          // generates the manifest file using the package.json informations
-          return Buffer.from(JSON.stringify({
-            description: process.env.npm_package_description,
-            version: process.env.npm_package_version,
-            ...JSON.parse(content.toString())
-          }))
-        }
+        from: "src/manifest.json"
       },
       {
         from: "src/_locales",
-        to: '_locales',
+        to: "_locales"
       }
     ]),
     new HtmlWebpackPlugin({
