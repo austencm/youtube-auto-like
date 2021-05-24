f1237a9ae590353bb4fbd4037b3ae32ba079db16
Austen Morgan <austen@boundaryla.com>
2021-05-23 18:42:26 -0700

    Fix like button visual issue after click

--- src/js/content/liker.js
+++ src/js/content/liker.js
@@ -2,8 +2,10 @@
  * Likes YouTube videos
  */
 const selectors = {
-  iconLike: 'ytd-video-primary-info-renderer g path[d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"]',
-  iconDislike: 'ytd-video-primary-info-renderer g path[d="M15 3H6c-.83 0-1.54.5-1.84 1.22l-3.02 7.05c-.09.23-.14.47-.14.73v1.91l.01.01L1 14c0 1.1.9 2 2 2h6.31l-.95 4.57-.03.32c0 .41.17.79.44 1.06L9.83 23l6.59-6.59c.36-.36.58-.86.58-1.41V5c0-1.1-.9-2-2-2zm4 0v12h4V3h-4z"]',
+  likeButton:
+    '#menu-container #top-level-buttons-computed > ytd-toggle-button-renderer:first-child yt-icon-button',
+  dislikeButton:
+    '#menu-container #top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(2) yt-icon-button',
   subscribeButton: '#subscribe-button tp-yt-paper-button',
 };
 
@@ -55,15 +57,15 @@ export default class Liker {
   waitForButtons() {
     this.log('waiting for buttons...');
 
-		return new Promise(resolve => {
+    return new Promise((resolve) => {
       const interval = setInterval(() => {
-        const iconLike = document.querySelector(selectors.iconLike);
-    		const iconDislike = document.querySelector(selectors.iconDislike);
-        // Make sure both icons exist
-        if (iconLike && iconDislike) {
-          // Find and store closest buttons
-          this.cache.likeButton = iconLike.closest('yt-icon-button');
-          this.cache.dislikeButton = iconDislike.closest('yt-icon-button');
+        const likeButton = document.querySelector(selectors.likeButton);
+        const dislikeButton = document.querySelector(selectors.dislikeButton);
+        // Make sure both buttons exist
+        if (likeButton && dislikeButton) {
+          // Store buttons
+          this.cache.likeButton = likeButton;
+          this.cache.dislikeButton = dislikeButton;
 
           this.log('...buttons ready');
           clearInterval(interval);
@@ -79,7 +81,7 @@ export default class Liker {
   waitForVideo() {
     this.log('waiting for video...');
 
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       const interval = setInterval(() => {
         this.cache.video = document.querySelector('.video-stream');
         // Does the video exist?
@@ -87,7 +89,7 @@ export default class Liker {
           this.log('...video ready');
           clearInterval(interval);
           resolve();
-    		}
+        }
       }, 1000);
     });
   }
@@ -97,10 +99,8 @@ export default class Liker {
    */
   isVideoRated() {
     return (
-      (
-        this.cache.likeButton.classList.contains('style-default-active') &&
-        !this.cache.likeButton.classList.contains('size-default')
-      ) ||
+      (this.cache.likeButton.classList.contains('style-default-active') &&
+        !this.cache.likeButton.classList.contains('size-default')) ||
       this.cache.dislikeButton.classList.contains('style-default-active')
     );
   }
@@ -111,7 +111,8 @@ export default class Liker {
    */
   isUserSubscribed() {
     // Select the sub button
-    const subscribeButton = this.cache.subscribeButton || document.querySelector(selectors.subscribeButton);
+    const subscribeButton =
+      this.cache.subscribeButton || document.querySelector(selectors.subscribeButton);
     // Does the button exist?
     if (!subscribeButton) return false;
     // Is the button active?
@@ -130,9 +131,12 @@ export default class Liker {
   }
 
   isAdPlaying() {
-    return this.cache.video && ['ad-showing', 'ad-interrupting'].every(c => {
-      return this.cache.video.closest('.html5-video-player').classList.contains(c);
-    });
+    return (
+      this.cache.video &&
+      ['ad-showing', 'ad-interrupting'].every((c) => {
+        return this.cache.video.closest('.html5-video-player').classList.contains(c);
+      })
+    );
   }
 
   /**
@@ -172,14 +176,14 @@ export default class Liker {
       case 'timed': {
         await this.waitForVideo();
         const { video } = this.cache;
-        const onVideoTimeUpdate = e => {
+        const onVideoTimeUpdate = (e) => {
           if (this.isAdPlaying()) return;
           // Are we 2 mins in or at the end of the video?
           if (video.currentTime >= 2 * 60 || video.currentTime >= video.duration) {
             this.clickLike();
             video.removeEventListener('timeupdate', onVideoTimeUpdate);
           }
-        }
+        };
         video.addEventListener('timeupdate', onVideoTimeUpdate);
         break;
       }
@@ -187,14 +191,14 @@ export default class Liker {
       case 'percent': {
         await this.waitForVideo();
         const { video } = this.cache;
-        const onVideoTimeUpdate = e => {
+        const onVideoTimeUpdate = (e) => {
           if (this.isAdPlaying()) return;
           // Are we more than 50% through the video?
           if (video.currentTime / video.duration >= 0.5) {
             this.clickLike();
             video.removeEventListener('timeupdate', onVideoTimeUpdate);
           }
-        }
+        };
         video.addEventListener('timeupdate', onVideoTimeUpdate);
         break;
       }
