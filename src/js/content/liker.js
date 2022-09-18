2521165645f1b38fd44e844e54b24b06846bbcd5
Austen Morgan <austen@boundaryla.com>
2022-09-18 14:20:01 -0700

    Support multiple button selectors

--- src/js/content/liker.js
+++ src/js/content/liker.js
@@ -2,11 +2,13 @@
  * Likes YouTube videos
  */
 const selectors = {
-  likeButton:
+  likeButton: [
     '#top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(1) yt-icon-button',
-  dislikeButton:
+  ],
+  dislikeButton: [
     '#top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(2) yt-icon-button',
-  subscribeButton: '#subscribe-button tp-yt-paper-button',
+  ],
+  subscribeButton: ['#subscribe-button tp-yt-paper-button'],
 };
 
 export default class Liker {
@@ -29,7 +31,9 @@ export default class Liker {
     /*
     We're hooking into YouTube's custom events to determine when the page changes.
      */
-    document.querySelector('ytd-app').addEventListener('yt-page-data-updated', this.start);
+    document
+      .querySelector('ytd-app')
+      .addEventListener('yt-page-data-updated', this.start);
 
     this.start();
   }
@@ -59,8 +63,10 @@ export default class Liker {
 
     return new Promise((resolve) => {
       const interval = setInterval(() => {
-        const likeButton = document.querySelector(selectors.likeButton);
-        const dislikeButton = document.querySelector(selectors.dislikeButton);
+        const likeButton = document.querySelectorAll(selectors.likeButton)[0];
+        const dislikeButton = document.querySelectorAll(
+          selectors.dislikeButton
+        )[0];
         // Make sure both buttons exist
         if (likeButton && dislikeButton) {
           // Store buttons
@@ -112,7 +118,8 @@ export default class Liker {
   isUserSubscribed() {
     // Select the sub button
     const subscribeButton =
-      this.cache.subscribeButton || document.querySelector(selectors.subscribeButton);
+      this.cache.subscribeButton ||
+      document.querySelectorAll(selectors.subscribeButton)[0];
     // Does the button exist?
     if (!subscribeButton) return false;
     // Is the button active?
@@ -134,7 +141,9 @@ export default class Liker {
     return (
       this.cache.video &&
       ['ad-showing', 'ad-interrupting'].every((c) => {
-        return this.cache.video.closest('.html5-video-player').classList.contains(c);
+        return this.cache.video
+          .closest('.html5-video-player')
+          .classList.contains(c);
       })
     );
   }
@@ -180,7 +189,10 @@ export default class Liker {
         const onVideoTimeUpdate = (e) => {
           if (this.isAdPlaying()) return;
           // Are we more than the chosen mins in or at the end of the video?
-          if (video.currentTime >= minutes * 60 || video.currentTime >= video.duration) {
+          if (
+            video.currentTime >= minutes * 60 ||
+            video.currentTime >= video.duration
+          ) {
             this.clickLike();
             video.removeEventListener('timeupdate', onVideoTimeUpdate);
           }
